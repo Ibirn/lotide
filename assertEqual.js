00f15e9cdaa47ce7d61438c07c611e4797b2c8b8
Ibirn <kmcricciuti@gmail.com>
2020-08-17 22:45:31 +0000

    modified to include template literal

--- assertEqual.js
+++ assertEqual.js
@@ -2,7 +2,7 @@ const assertEqual = (actual, expected) => {
   if (actual === expected) {
     console.log("👍 Assertion Passed.");
   } else {
-    console.log("😱 Assertion Failed: " + actual + " !== " + expected);
+    console.log(`😱 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 assertEqual("Butts", "Butts");
