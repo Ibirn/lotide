5c18a21f70913731b6c91bbac2cfc599c276fb20
Ibirn <kmcricciuti@gmail.com>
2020-08-18 20:07:17 +0000

    Deleted testing code

--- joinList.js
+++ joinList.js
@@ -8,8 +8,3 @@ const joinList = array => {
   }
   return output;
 };
-
-// Test / Driver Code below...
-const conceptList = ["gists", "types", "operators", "iteration", "problem solving"];
-const concepts = joinList(conceptList);
-console.log(`Today I learned about ${concepts}.`);
