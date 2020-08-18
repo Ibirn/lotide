026352700ff334549c81c546ec556e00c7904810
Ibirn <kmcricciuti@gmail.com>
2020-08-18 22:27:26 +0000

    Removed array assert code. Whoops.

--- eqArrays.js
+++ eqArrays.js
@@ -1,11 +1,3 @@
-const assertEqual = (actual, expected) => {
-  if (actual === expected) {
-    console.log("👍 Assertion Passed.");
-  } else {
-    console.log(`😱 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const eqArrays = (arrayOne, arrayTwo) => {
   if (arrayOne.length !== arrayTwo.length){
     return false;
