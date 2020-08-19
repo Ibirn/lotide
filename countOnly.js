afb3a256c6df71ad8ab14af91ac1590afcf9d29d
Ibirn <kmcricciuti@gmail.com>
2020-08-19 20:27:31 +0000

    linted, and testing code was return in comments to remind me how it functions in review.

--- countOnly.js
+++ countOnly.js
@@ -1,8 +1,15 @@
+// const assertEqual = (actual, expected) => {
+//   if (actual === expected) {
+//     console.log("👍 Assertion Passed.");
+//   } else {
+//     console.log(`😱 Assertion Failed: ${actual} !== ${expected}`);
+//   }
+// };
 //returns an object containing the counts(values) of things passed in the array. so [a,a] + {a:true} => {a:2}
 const countOnly = (allItems, itemstoCount) => {
   const results = {};
   for (let key of allItems) {
-    console.log(key)
+    console.log(key);
     if (itemstoCount[key]) {
       if (!results[key]) {
         results[key] = 1;
@@ -12,4 +19,22 @@ const countOnly = (allItems, itemstoCount) => {
     }
   }
   return results;
-}
+};
+
+// const firstNames = [
+//   "Karl",
+//   "Salima",
+//   "Agouhanna",
+//   "Fang",
+//   "Kavith",
+//   "Jason",
+//   "Salima",
+//   "Fang",
+//   "Joe"
+// ];
+
+// const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true });
+
+// assertEqual(result1["Jason"], 1);
+// assertEqual(result1["Karima"], undefined);
+// assertEqual(result1["Fang"], 2);
