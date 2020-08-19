d194edfc32fa0e53f019b35150c9436adc64dfd7
Ibirn <kmcricciuti@gmail.com>
2020-08-19 00:34:00 +0000

    went through linting.

--- without.js
+++ without.js
@@ -1,5 +1,5 @@
 const without = (source, remove) => {
   let output = source.filter(number => !remove.includes(number));
   return output;
-}
-//  console.log(without(["cat", "dog", "human" ], [2, "cat", true])) // => [2, 3]
+};
+//console.log(without(["cat", "dog", "human" ], [2, "cat", true])); // => [2, 3]
