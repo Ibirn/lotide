e33e272d0d19d9d2835bd1ed563365b813944ce2
Ibirn <kmcricciuti@gmail.com>
2020-08-19 00:29:51 +0000

    simple way to filter elements of one array out of another.

---
+++ without.js
@@ -0,0 +1,5 @@
+const without = (source, remove) => {
+  let output = source.filter(number => !remove.includes(number));
+  return output;
+}
+//  console.log(without(["cat", "dog", "human" ], [2, "cat", true])) // => [2, 3]
