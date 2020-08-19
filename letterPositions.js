7b46a51cfecb2acd6738e8763446772aadd5325f
Ibirn <kmcricciuti@gmail.com>
2020-08-19 21:02:13 +0000

    function to gather index positions of letters in an argument (excluding spaces)

---
+++ letterPositions.js
@@ -0,0 +1,45 @@
+// const assertEqualArrays = (actual, expected) => {
+//   const eqArrays = (arrayOne, arrayTwo) => {
+//     if (arrayOne.length !== arrayTwo.length) {
+//       return false;
+//     }
+//     for (let i = 0; i < arrayOne.length; i++) {
+//       if (arrayOne[i] !== arrayTwo[i]) {
+//         return false;
+//       }
+//     }
+//     return true;
+//   };
+//   (eqArrays(actual, expected)) ? console.log("👍 Assertion Passed.") : console.log(`😱 Assertion Failed: ${actual} !== ${expected}`);
+// };
+
+// const eqArrays = (arrayOne, arrayTwo) => {
+//   if (arrayOne.length !== arrayTwo.length){
+//     return false;
+//   }
+//   for (let i = 0; i < arrayOne.length; i++) {
+//     if (arrayOne[i] !== arrayTwo[i]){
+//       return false;
+//     }
+//   }
+//   return true;
+// };
+
+const letterPositions = input => {
+  let output = {}
+  for (let key in input) {
+    if (input[key] === ' ') {
+    } else {
+      if (!output[input[key]]) {
+        output[input[key]] = [Number(key)];
+      } else {
+        output[input[key]].push(Number(key));
+      }
+    }
+  }
+  return output;
+};
+
+// console.log(letterPositions("lighthouse in the house").u) 
+
+// assertEqualArrays(letterPositions("hello").e, [1]);
