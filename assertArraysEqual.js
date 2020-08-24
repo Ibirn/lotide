b69bae3c3517bb22937f9c3a8f36d3fba8f9ffbb
Ibirn <kmcricciuti@gmail.com>
2020-08-24 20:21:47 +0000

    refactored for module use.

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,17 +1,7 @@
 const assertEqual = require("./assertEqual");
+const eqArrays = require("./eqArrays")
 
 const assertArraysEqual = (actual, expected) => {
-  const eqArrays = (arrayOne, arrayTwo) => {
-    if (arrayOne.length !== arrayTwo.length) {
-      return false;
-    }
-    for (let i = 0; i < arrayOne.length; i++) {
-      if (arrayOne[i] !== arrayTwo[i]) {
-        return false;
-      }
-    }
-    return true;
-  };
   (eqArrays(actual, expected)) ? console.log("👍 Assertion Passed.") : console.log(`😱 Assertion Failed: ${actual} !== ${expected}`);
 };
 
