4ffbb0740dfd2f77beac797bc15b49d841fe5781
Ibirn <kmcricciuti@gmail.com>
2020-08-24 20:11:33 +0000

    refactored for use with modules.

--- eqArrays.js
+++ eqArrays.js
@@ -1,3 +1,6 @@
+const assertEqual = require("./assertEqual")
+//const eqObjects = require("./eqObjects") //This chunk and below commented section is for working in tandem with the eqObjects function but causes errors when not in same JS file. 
+
 const eqArrays = (arrOne, arrTwo) => {
   if (arrOne.length !== arrTwo.length) {
     return false;
@@ -7,13 +10,15 @@ const eqArrays = (arrOne, arrTwo) => {
       if (!eqArrays(arrOne[i], arrTwo[i])) {
         return false;
       }
-    } else if (typeof arrOne[i] === 'object' && arrOne[i] !== 'null' && !Array.isArray(arrOne[i])) {
-      if (!eqObjects(arrOne[i],arrTwo[i])) {
-        return false;
-      }
+    // } else if (typeof arrOne[i] === 'object' && arrOne[i] !== 'null' && !Array.isArray(arrOne[i])) {
+    //   if (!eqObjects(arrOne[i],arrTwo[i])) {
+    //     return false;
+    //   } 
     } else if (arrOne[i] !== arrTwo[i]) {
       return false;
     }
   }
   return true;
 };
+
+module.exports = eqArrays;
