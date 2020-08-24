d9385303c360725ba194b114d2bd8cbd36f5b5e2
Ibirn <kmcricciuti@gmail.com>
2020-08-24 21:22:57 +0000

    refactored for use with mocha chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,11 +1,31 @@
-const assertArraysEqual = require("../assertArraysEqual");
+const assert = require("chai").assert
 const middle = require("../middle");
 
-console.log(middle([1, 2, 4, 5, 6, "kitten", false, "cheese", 3, -9]))
-console.log(middle([1,2,3,4,5]))
-console.log(middle([1])) // => []
-console.log(middle([1, 2])) // => []
-console.log(middle([1, 2, 3, 4])) // => [2, 3]
-console.log(middle([1, 2, 3, 4, 5, 6])) // => [3, 4]
+describe("#middle", () => {
+  it("returns ['kitten'] when passed  [1, 2, 4, 5, 6, 'kitten', false, 'cheese', 3, true, -9]", () => {
+    assert.deepEqual(middle([1, 2, 4, 5, 6, "kitten", false, "cheese", 3, true, -9]), ['kitten'])
+  });
+  it("returns [3] when passed [1, 2, 3, 4, 5]", () => {
+    assert.deepEqual(middle([1,2,3,4,5]), [3])
+  });
+  it("returns an empty array when passed a single digit array", () => {
+    assert.deepEqual(middle(['ham']), [])
+  });
+  it("returns an empty array when passed a two digit array", () => {
+    assert.deepEqual(middle([true, false]), [])
+  });
+  it("passes middle two elements when passed even length array", () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2,3])
+    assert.deepEqual(middle(["Jan", "Feb", "Mar", "Apr", "May", "Jun"]), ["Mar", "Apr"])
+  });
+  
+});
 
-assertArraysEqual(middle([1,2,"3",4,5,6]),["3",4])
+
+// console.log(middle([1,2,3,4,5]));
+// console.log(middle([1])); // => []
+// console.log(middle([1, 2])); // => []
+// console.log(); // => [2, 3]
+// console.log(middle([1, 2, 3, 4, 5, 6])); // => [3, 4]
+
+// assertArraysEqual(middle([1,2,"3",4,5,6]),["3",4]);
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,6 +1,25 @@
-const tail = require("../tail")
-const assertEqual = require("../assertEqual")
+const tail = require("../tail");
+const assert = require("chai").assert;
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+describe("#tail", () => {
+  it("returns [2, 3] for [1, 2, 3]", () => {
+    assert.deepEqual(tail([1, 2, 3]), [2, 3]);
+  });
+  it("returns empty array for ['5']", ()=>{
+    assert.deepEqual(tail(['5']), []);
+  });
+  it("returns ['Lighthouse', Labs'] for ['Yo Yo', 'Lighthouse', 'Labs']", ()=>{
+    assert.deepEqual(tail(['Yo Yo', 'Lighthouse', 'Labs']), ['Lighthouse', 'Labs']);
+  });
+  it("does not modify original array when returning value", ()=>{
+    const words = ["Yo Yo", "Lighthouse", "Labs"];
+    assert.strictEqual(tail(words).length, 2);
+    assert.strictEqual(words.length, 3);
+  });
+
+});
+
+
+// const words = ["Yo Yo", "Lighthouse", "Labs"];
+// tail(words); // no need to capture the return value since we are not checking it
+// assertEqual(words.length, 3); // original array should still have 3 elements!
