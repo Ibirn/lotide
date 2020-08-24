4f6d397536fbce52a8e5959f91942f7703e1c89e
Ibirn <kmcricciuti@gmail.com>
2020-08-24 20:29:55 +0000

    refactored for module use.

--- middle.js
+++ middle.js
@@ -1,29 +1,3 @@
-// const assertEqualArrays = (actual, expected) => {
-//   const eqArrays = (arrayOne, arrayTwo) => {
-//     if (arrayOne.length !== arrayTwo.length) {
-//       return false;
-//     }
-//     for (let i = 0; i < arrayOne.length; i++) {
-//       if (arrayOne[i] !== arrayTwo[i]) {
-//         return false;
-//       }
-//     }
-//     return true;
-//   };
-//   (eqArrays(actual, expected)) ? console.log("👍 Assertion Passed.") : console.log(`😱 Assertion Failed: ${actual} !== ${expected}`);
-// };
-
-// const eqArrays = (arrayOne, arrayTwo) => {
-//   if (arrayOne.length !== arrayTwo.length) {
-//     return false;
-//   }
-//   for (let i = 0; i < arrayOne.length; i++) {
-//     if (arrayOne[i] !== arrayTwo[i]) {
-//       return false;
-//     }
-//   }
-//   return true;
-// };
 const middle = array => {
   let middleElements = [];
   if (array.length <= 2) {
@@ -38,11 +12,4 @@ const middle = array => {
   return middleElements;
 };
 
-// console.log(middle([1, 2, 4, 5, 6, "kitten", false, "cheese", 3, -9]))
-// console.log(middle([1,2,3,4,5]))
-// console.log(middle([1])) // => []
-// console.log(middle([1, 2])) // => []
-// middle([1, 2, 3, 4]) // => [2, 3]
-// middle([1, 2, 3, 4, 5, 6]) // => [3, 4]
-
-// assertEqualArrays(middle([1,2,"3",4,5,6]),["3",4])
+module.exports = middle;
